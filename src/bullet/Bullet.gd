51137c6f4988644d306aeff21111a8633dd3d2a4
Fernando Henrique <fernandohjunqueira@protonmail.com>
2021-06-03 11:37:50 -0300

    Bullet kills Enemy on collision

--- src/bullet/Bullet.gd
+++ src/bullet/Bullet.gd
@@ -14,10 +14,14 @@ func _physics_process(delta):
 	var velocity = direction * speed * delta;
 	velocity = move_and_collide(velocity);
 
-
+# Destrói o projétil em caso de colisão
+# Se colidir com um inimigo, destrói o inimigo também.
 func _on_Hitbox_body_entered(body):
+	if body != null and body.is_in_group("enemies"):
+		body.queue_free();
 	queue_free();
 
 
+# Destrói o projétil após o tempo determinado no Timer.
 func _on_Timer_timeout():
 	queue_free();
