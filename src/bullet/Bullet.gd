f3d41c9222942c817625e7a789c43c510b05793a
Fernando Henrique <fernandohjunqueira@protonmail.com>
2021-06-03 11:32:54 -0300

    Bullet dies on collision or timeout

--- src/bullet/Bullet.gd
+++ src/bullet/Bullet.gd
@@ -13,3 +13,11 @@ func _ready():
 func _physics_process(delta):
 	var velocity = direction * speed * delta;
 	velocity = move_and_collide(velocity);
+
+
+func _on_Hitbox_body_entered(body):
+	queue_free();
+
+
+func _on_Timer_timeout():
+	queue_free();
